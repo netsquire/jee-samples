ef4820a64315fdb6d96834867917e4e8f26fa180
Valerii Sorokin <vsorokin@redhat.com>
2016-07-17 23:40:20 +0200

    Commit

--- src/up/HelloWorld.java
+++ src/up/HelloWorld.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet({ "/HelloWorld", "/helloworld", "/sample", "/" })
+@WebServlet({ "/HelloWorld", "/helloworld", "/sample", "ornhu" })
 public class HelloWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
